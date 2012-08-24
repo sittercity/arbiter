a9a0f59b98d6920ae985a80a22871e2e49b374be
Sam de Freyssinet <sdefreyssinet@sittercity.com>
2012-08-24 14:21:56 -0500

    --amend

---
+++ lib/arbiter.rb
@@ -0,0 +1,24 @@
+class Arbiter
+
+  @queue = :arbiter
+
+  def self.perform(message, metadata)
+    message = message.to_sym
+    if @message_table[message] and ! @message_table[message].empty?
+      @message_table[message].each do |listener|
+        listener.notify(message, metadata)
+      end
+    end
+  end
+
+  def self.set_listeners(listeners)
+    @message_table = {}
+    listeners.each do |listener|
+      listener.subscribe_to.each do |channel|
+        @message_table[channel] ||= []
+        @message_table[channel] << listener
+      end
+    end
+  end
+end
+
